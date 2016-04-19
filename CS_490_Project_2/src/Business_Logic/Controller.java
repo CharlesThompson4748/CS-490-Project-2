07a44342ee688e7abadf10a0a3f48d89c01447b8
Charles Thompson <charles.thompson4748@gmail.com>
2016-04-19 12:52:37 -0500

    Added New Class and Controller changes
    Added the Keyword class and made the Controller a Singleton

--- CS_490_Project_2/src/Business_Logic/Controller.java
+++ CS_490_Project_2/src/Business_Logic/Controller.java
@@ -10,5 +10,22 @@ package Business_Logic;
  * @author charlie
  */
 public class Controller {
+    private static Controller Singleton;
     
+    //Class Constructor
+    private Controller() {}
+
+    //Function to return the Singleton class object
+    //Input: None
+    //Output: Class Singleton object
+    public static Controller instance() {
+        //Determine if a class object has been created.
+        if(Singleton == null) {
+            //No class object created yet. Create one.
+            Singleton = new Controller();
+        }
+        //Return the Singleton class object
+        return Singleton;
+    }
+
 }
